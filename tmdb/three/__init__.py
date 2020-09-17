2966824c410ce9902e4b964e2355754941e295d9
Zeheng Li <imzehengl@gmail.com>
2020-09-17 00:04:32 -0600

    en able global options

--- tmdb/three/__init__.py
+++ tmdb/three/__init__.py
@@ -54,11 +54,28 @@ mixins = [
 
 
 class TMDb(*mixins):
-    def __init__(self, api_key):
+    def __init__(self, api_key, **kwargs):
         self.api_key = api_key
+        self.options = kwargs
         self.session = requests.Session()
         self.session.headers.update({"Content-Type": "application/json;charset=utf-8"})
 
-    def make_request(self, method, url, params, payload=None):
-        params.update({"api_key": self.api_key})
+    def make_request(self, method, url, request_params, payload=None):
+        params = {"api_key": self.api_key}
+        params.update(self.options)
+        params.update(request_params)
+
+        # https://developers.themoviedb.org/3/getting-started/image-languages
+        # convert keyword argument: (..., include_image_language=["en", "null"])
+        # into url param: ...&include_image_language=en,null
+        for key in params:
+            if type(params[key]) is list:
+                params[key] = ",".join(params[key])
+
         return self.session.request(method, url, params=params, json=payload)
+
+    def set_options(self, **kwargs):
+        self.options.update(kwargs)
+
+    def reset_options(self):
+        self.options = {}
