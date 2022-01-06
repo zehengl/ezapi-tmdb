a4872480332762b9eba6239c70e6337b041ab6ea
Zeheng Li <imzehengl@gmail.com>
2022-01-06 05:38:27 -0700

    fix desc

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+from io import open
 from os import path
 from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, "README.md")) as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 with open(path.join(here, "requirements.txt")) as f:
