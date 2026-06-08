45639843c881009d4f73627823f2a2fd14c43f0d
Zeheng Li <imzehengl@gmail.com>
2026-06-08 14:49:00 -0600

    Add xfail marker to guest session tests
    Mark guest session tests as expected to fail.

--- tests/three/test_guest_sessions.py
+++ tests/three/test_guest_sessions.py
@@ -3,18 +3,21 @@ from utils import polite
 
 
 @polite
+@pytest.mark.xfail
 def test_get_guest_session_rated_movies(tmdb):
     guest_session_id = tmdb.create_guest_session().get("guest_session_id")
     assert tmdb.get_guest_session_rated_movies(guest_session_id) is not None
 
 
 @polite
+@pytest.mark.xfail
 def test_get_guest_session_rated_tvs(tmdb):
     guest_session_id = tmdb.create_guest_session().get("guest_session_id")
     assert tmdb.get_guest_session_rated_tvs(guest_session_id) is not None
 
 
 @polite
+@pytest.mark.xfail
 def test_get_guest_session_rated_tv_episodes(tmdb):
     guest_session_id = tmdb.create_guest_session().get("guest_session_id")
     assert tmdb.get_guest_session_rated_tv_episodes(guest_session_id) is not None
